dddbd18bc74b1a9939808a8f1af73f8cfa2b33e5
ddbit <dcarboni@gmail.com>
2019-12-15 22:14:04 +0100

    consolidating contract

--- contracts/escrow.sol
+++ contracts/escrow.sol
@@ -2,32 +2,41 @@
 pragma solidity 0.5;
 contract Escrow{
     address arbiter;
-    address customer;
-    address supplier;
+    address payable customer;
+    address payable supplier;
     bool UNCLAIMED  = true;
     bool CLAIMED    = false;
     bool APPROVED   = false;
     bool PAID       = false;
-    address claimer;
+    address payable claimer;
     
     
-    function claim(){
+    constructor(address payable _customer, 
+                address payable _supplier,
+                address payable _arbiter) public{
+
+        customer = _customer;
+        supplier = _supplier;
+        arbiter  = _arbiter;
+    }
+
+    function claim() external{
         require(UNCLAIMED);
         claimer = msg.sender;
         CLAIMED = true;
     }
     
     
-    function approve(){
+    function approve() external{
         require(CLAIMED);
         require(msg.sender != claimer);
         APPROVED = true;
     }
     
-    function withdraw(){
+    function withdraw() external{
         require (APPROVED);
         require (msg.sender == claimer);
-        claimer.send()
+        claimer.transfer(address(this).balance);
         
     }
     
