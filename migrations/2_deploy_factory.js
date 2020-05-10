99d7971dae7c098f51c5d30758d7d8dde3de024f
davide <dcarboni@gmail.com>
2020-05-10 13:02:10 +0200

    typo fixed in comment

--- migrations/2_deploy_factory.js
+++ migrations/2_deploy_factory.js
@@ -1,7 +1,6 @@
 const EscrowFactory = artifacts.require("EscrowFactory");
 
 module.exports = function(deployer,network,accounts) {
-  //change the address with the actual VALOR token address
   console.log("network:"+network);
   if(network == "ropsten"){
   	deployer.deploy(EscrowFactory);
